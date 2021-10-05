7d35e3f0fc5342c4f9919747d8942b727b64d673
Majeed-Omer <91630712+Majeed-Omer@users.noreply.github.com>
2021-10-05 18:50:16 +0300

    Add files via upload

---
+++ ticket.java
@@ -0,0 +1,19 @@
+package m;
+
+public class ticket {
+	
+	int type = 2;
+	String names = "1- Normal\n2- VIPE";
+	public static void detailType1() {
+	
+	}
+    public static void detailType2() {
+		
+	}
+    public static void buyTicket1() {
+    	
+	}
+    public static void buyTicket2() {
+		
+	}
+}
