3435603e898617291bea74ba1f5a5afaef4aa05d
Yahya-Mewan <91800870+Yahya-Mewan@users.noreply.github.com>
2021-10-11 22:10:15 +0300

    Add files via upload

---
+++ Source Code/login.java
@@ -0,0 +1,16 @@
+public class login {
+    private static String email = "";
+    private static String password = "";
+    public static void setEmail(String e) {
+        email = e;
+    }
+    public static void setPassword(String p) {
+        password = p;
+    }
+    public static String getEmail() {
+        return email;
+    }
+    public static String getPassword() {
+        return password;
+    }
+}
