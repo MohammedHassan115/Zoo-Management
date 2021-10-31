027b11f0570691011a008872981d43e1dbaa5439
Majeed-Omer <91630712+Majeed-Omer@users.noreply.github.com>
2021-10-31 20:40:47 +0300

    edited
    this edited

--- source/Zoo.java
+++ source/Zoo.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 
 public class Zoo {
     public static void main(String args[]) throws IOException {
-    	view v = new view();
-    	v.PU();
+    	new view();
     }
 }
