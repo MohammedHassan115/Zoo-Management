ffcaff7b995e1f8edfd38580663ab837f2972f21
Yahya-Mewan <91800870+Yahya-Mewan@users.noreply.github.com>
2021-11-06 20:25:17 +0300

    Update incomeModel.java

--- source/Models/incomeModel.java
+++ source/Models/incomeModel.java
@@ -1,1 +1,21 @@
+package m;
 
+import java.util.Scanner;
+
+public class incomeModel {
+	static Scanner s = new Scanner(System.in);
+	static int price = 0;
+	public static void updateI(int p) {
+		price += p;
+		income.managerActions();
+	}
+    public static void clearI() {
+		price = 0;
+		System.out.println("The income is: " + price + "$\n");
+		income.managerActions();
+	}
+    public static void viewI() {
+		System.out.println("The income is: " + price + "$\n");
+		income.managerActions();
+	}
+}
