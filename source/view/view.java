c5970df681f09229654c53c7aea8c280ceddf702
Majeed-Omer <91630712+Majeed-Omer@users.noreply.github.com>
2021-11-06 23:46:22 +0300

    edited

--- source/view/view.java
+++ source/view/view.java
@@ -1,6 +1,11 @@
 package m;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Optional;
 import java.util.Scanner;
 
@@ -8,29 +13,53 @@ public class view {
 	static String username = "";
 	static String password = "";
 	
-	public view() throws IOException{
-		Scanner s = new Scanner(System.in);
+	public view() throws IOException {
+		Scanner ss = new Scanner(System.in);
+	    Socket echoSocket = null;
+		PrintWriter out = null;
+		BufferedReader in = null;
+		
 		System.out.println("Please enter username and password:");
     	System.out.print("Username: ");
-    	login.setUsername(s.nextLine());
+    	login.setUsername(ss.nextLine());
+    	username = Optional.ofNullable(login.getUsername()).orElse("unknown");
     	System.out.print("Password: ");
-    	login.setPassword(s.nextLine());
+    	login.setPassword(ss.nextLine());
+    	password = Optional.ofNullable(login.getPassword()).orElse("0000");
     	System.out.println();
+    	try {
+    		echoSocket = new Socket("127.0.0.1", 7777); 
+    		out = new PrintWriter(echoSocket.getOutputStream(), true);
+    		in=new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+    		} catch (UnknownHostException ee) {
+    		System.err.println("Don't know host: toshiba.");
+    		System.exit(1);
+    			} catch (IOException ei) {
+    			System.err.println("Couldn't get I/O for "
+    			+ "the connection to: toshiba.");
+    			System.exit(1);
+    			}
+    			String userInput = username + " " + password;
+    			System.out.println(in.readLine());
+    			out.println(userInput);
+    			System.out.println("echo: " + in.readLine() + "\n");
+    			out.close();
+    			in.close();
+    			echoSocket.close();
     	animal a = new animal();
     	employee e = new employee();
     	supply sp = new supply();
     	income i = new income();
     	expenditure ex = new expenditure();
     	ticket t = new ticket();
-    	username = Optional.ofNullable(login.getUsername()).orElse("unknown");
-    	password = Optional.ofNullable(login.getPassword()).orElse("0000");
+    	
     	String actions = "";
     	if(username.equals("mohammed123") && password.equals("12345!@#$%")) {
     		System.out.println("Hello manager\n");
     		while(!actions.equals("exit")) {
         		display_options1();
         		System.out.println("Select an action: ");
-                actions = s.next().toLowerCase();
+                actions = ss.next().toLowerCase();
                 System.out.println();    
         	switch(actions){
         	case "animal":
@@ -67,7 +96,7 @@ public class view {
         	while(!actions.equals("exit")) {
     		display_options2();
     		System.out.println("Select an action: ");
-            actions = s.next().toLowerCase();
+            actions = ss.next().toLowerCase();
             System.out.println();    
             switch(actions){
             case "animal":
@@ -87,7 +116,6 @@ public class view {
     	}
     }
     }
-    		
 	}
 	public static void display_options1(){
         System.out.println("Options:");
