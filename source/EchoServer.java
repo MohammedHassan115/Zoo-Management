a31ea58867898c85bca9d809b34de95afc957862
Majeed-Omer <91630712+Majeed-Omer@users.noreply.github.com>
2021-11-06 23:47:34 +0300

    Add files via upload

---
+++ source/EchoServer.java
@@ -0,0 +1,42 @@
+package m;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+public static void main(String[] args) throws IOException{
+ServerSocket serverSocket = null;
+try {
+serverSocket = new ServerSocket(7777);
+} catch (IOException e) {
+System.err.println("Could not listen on port: 7777.");
+System.exit(1);
+}
+Socket clientSocket = null;
+try {
+clientSocket = serverSocket.accept();
+} catch (IOException e) {
+System.err.println("Accept failed.");
+System.exit(1);
+}
+PrintWriter out = new PrintWriter(
+clientSocket.getOutputStream(), true);
+BufferedReader in = new BufferedReader( new
+InputStreamReader(clientSocket.getInputStream()));
+String inputLine, outputLine;
+out.println("Welcome to Echo Server, your username and password will save in to sever: ");
+while ((inputLine = in.readLine()) != null) {
+outputLine = inputLine;
+out.println(outputLine);
+}
+out.close();
+in.close();
+clientSocket.close();
+serverSocket.close();
+	}
+
+}
