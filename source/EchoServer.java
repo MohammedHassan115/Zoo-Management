903b4133b7a0fff2a6c6fe5ae6255804c807e016
Majeed-Omer <91630712+Majeed-Omer@users.noreply.github.com>
2021-11-09 00:05:25 +0300

    edited

--- source/EchoServer.java
+++ source/EchoServer.java
@@ -1,4 +1,4 @@
-package m;
+package pop;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,36 +7,37 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class EchoServer {
-public static void main(String[] args) throws IOException{
-ServerSocket serverSocket = null;
-try {
-serverSocket = new ServerSocket(7777);
-} catch (IOException e) {
-System.err.println("Could not listen on port: 7777.");
-System.exit(1);
-}
-Socket clientSocket = null;
-try {
-clientSocket = serverSocket.accept();
-} catch (IOException e) {
-System.err.println("Accept failed.");
-System.exit(1);
-}
-PrintWriter out = new PrintWriter(
-clientSocket.getOutputStream(), true);
-BufferedReader in = new BufferedReader( new
-InputStreamReader(clientSocket.getInputStream()));
-String inputLine, outputLine;
-out.println("Welcome to Echo Server, your username and password will save in to sever: ");
-while ((inputLine = in.readLine()) != null) {
-outputLine = inputLine;
-out.println(outputLine);
-}
-out.close();
-in.close();
-clientSocket.close();
-serverSocket.close();
+public class serverSocket {
+	public static void main(String args[]) throws IOException {
+		ServerSocket serverSocket = null;
+		try {
+		serverSocket = new ServerSocket(7777);
+		} catch (IOException e) {
+		System.err.println("Could not listen on port: 7777.");
+		System.exit(1);
+		}
+		Socket clientSocket = null;
+		try {
+		clientSocket = serverSocket.accept();
+		} catch (IOException e) {
+		System.err.println("Accept failed.");
+		System.exit(1);
+		}
+		PrintWriter out = new PrintWriter(
+		clientSocket.getOutputStream(), true);
+		BufferedReader in = new BufferedReader( new
+		InputStreamReader(clientSocket.getInputStream()));
+		String inputLine, outputLine;
+		out.println("Welcome to Echo Server:");
+		while ((inputLine = in.readLine()) != null) {
+		outputLine = inputLine;
+		out.println(outputLine);
+		if (outputLine.equals("bye")) break;
+		}
+		out.close();
+		in.close();
+		clientSocket.close();
+		serverSocket.close();
+		
 	}
-
 }
