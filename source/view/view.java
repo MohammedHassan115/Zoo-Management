8ed47938917995d5ffad7d7a968d3b81468ab53c
Majeed-Omer <91630712+Majeed-Omer@users.noreply.github.com>
2021-11-08 23:58:10 +0300

    edited

--- source/view/view.java
+++ source/view/view.java
@@ -1,11 +1,6 @@
 package m;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Optional;
 import java.util.Scanner;
 
@@ -15,9 +10,6 @@ public class view {
 	
 	public view() throws IOException {
 		Scanner ss = new Scanner(System.in);
-	    Socket echoSocket = null;
-		PrintWriter out = null;
-		BufferedReader in = null;
 		
 		System.out.println("Please enter username and password:");
     	System.out.print("Username: ");
@@ -27,35 +19,10 @@ public class view {
     	login.setPassword(ss.nextLine());
     	password = Optional.ofNullable(login.getPassword()).orElse("0000");
     	System.out.println();
-    	try {
-    		echoSocket = new Socket("127.0.0.1", 7777); 
-    		out = new PrintWriter(echoSocket.getOutputStream(), true);
-    		in=new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
-    		} catch (UnknownHostException ee) {
-    		System.err.println("Don't know host: toshiba.");
-    		System.exit(1);
-    			} catch (IOException ei) {
-    			System.err.println("Couldn't get I/O for "
-    			+ "the connection to: toshiba.");
-    			System.exit(1);
-    			}
-    			String userInput = username + " " + password;
-    			System.out.println(in.readLine());
-    			out.println(userInput);
-    			System.out.println("echo: " + in.readLine() + "\n");
-    			out.close();
-    			in.close();
-    			echoSocket.close();
-    	animal a = new animal();
-    	employee e = new employee();
-    	supply sp = new supply();
-    	income i = new income();
-    	expenditure ex = new expenditure();
-    	ticket t = new ticket();
     	
     	String actions = "";
     	if(username.equals("mohammed123") && password.equals("12345!@#$%")) {
-    		System.out.println("Hello manager\n");
+    		System.out.println("Hello manager");
     		while(!actions.equals("exit")) {
         		display_options1();
         		System.out.println("Select an action: ");
@@ -63,22 +30,25 @@ public class view {
                 System.out.println();    
         	switch(actions){
         	case "animal":
-                a.managerActions();
+                animal.managerActions();
             break;
         	case "employee":
-                e.managerActions();
+                employee.managerActions();
             break;
         	case "supply":
-                sp.managerActions();
+                supply.managerActions();
             break;
         	case "income":
-                i.managerActions();
+                income.managerActions();
             break;
         	case "expenditure":
-                ex.managerActions();
+                expenditure.managerActions();
             break;
         	case "ticket":
-                t.managerActions();
+                ticket.managerActions();
+            break;
+        	case "server":
+                EchoClient.client();
             break;
         	case "back":
                 new view();
@@ -100,10 +70,10 @@ public class view {
             System.out.println();    
             switch(actions){
             case "animal":
-            a.userActions();
+            animal.userActions();
             break;
             case "ticket":
-            t.userActions();
+            ticket.userActions();
             break;
             case "back":
                 new view();
@@ -118,14 +88,15 @@ public class view {
     }
 	}
 	public static void display_options1(){
-        System.out.println("Options:");
+        System.out.println("\nOptions:");
         System.out.println("1- animal - show you list of sub actions with animals");
         System.out.println("2- employee - show you list of sub actions with employees");
         System.out.println("3- supply - show you list of sub actions with supplies");
         System.out.println("4- income - show you list of sub actions with income");
         System.out.println("4- expenditure - show you list of sub actions with expenditure");
         System.out.println("5- ticket - show list of sub actions with tickets");
-        System.out.println("6- back - back to username and password place");
+        System.out.println("6- server - client will conect to server");
+        System.out.println("7- back - back to username and password place");
 
         System.out.println("--");
         System.out.println("options - display this options message");
@@ -135,7 +106,7 @@ public class view {
         System.out.println("Options:");
         System.out.println("1- animal - show you list of sub actions with animals");
         System.out.println("2- ticket - show list of sub actions with tickets");
-        System.out.println("3- back - back to username and password place");
+        System.out.println("4- back - back to username and password place");
 
         System.out.println("--");
         System.out.println("options - display this options message");
