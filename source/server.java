36b2f16b0b47185604b6fc1e89aec2dc20ac6470
Majeed-Omer <91630712+Majeed-Omer@users.noreply.github.com>
2021-11-16 02:10:11 +0300

    Add files via upload

---
+++ source/server.java
@@ -0,0 +1,14 @@
+package dd;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+	public static void main(String[] args) throws IOException{
+	
+	}
+}
