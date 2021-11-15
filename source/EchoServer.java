dcc1e73a6e326bd79e4c3adfc506a01f6d715e7b
Majeed-Omer <91630712+Majeed-Omer@users.noreply.github.com>
2021-11-15 22:00:56 +0300

    Update EchoServer.java

--- source/EchoServer.java
+++ source/EchoServer.java
@@ -1,6 +1,9 @@
 package pop;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -11,9 +14,9 @@ public class serverSocket {
 	public static void main(String args[]) throws IOException {
 		ServerSocket serverSocket = null;
 		try {
-		serverSocket = new ServerSocket(7777);
+		serverSocket = new ServerSocket(8188);
 		} catch (IOException e) {
-		System.err.println("Could not listen on port: 7777.");
+		System.err.println("Could not listen on port: 5432.");
 		System.exit(1);
 		}
 		Socket clientSocket = null;
@@ -27,17 +30,44 @@ public class serverSocket {
 		clientSocket.getOutputStream(), true);
 		BufferedReader in = new BufferedReader( new
 		InputStreamReader(clientSocket.getInputStream()));
-		String inputLine, outputLine;
-		out.println("Welcome to Echo Server:");
+		String outputLine = "";
+		String inputLine = "";
 		while ((inputLine = in.readLine()) != null) {
 		outputLine = inputLine;
-		out.println(outputLine);
-		if (outputLine.equals("bye")) break;
+		FileWriter fw = new FileWriter(outputLine + ".txt", true);
+		File f = new File("C:\\Users\\PC-BALEN\\eclipse-workspace\\mom\\" + outputLine + ".txt");
+		BufferedReader br = new BufferedReader(new FileReader(f));
+		BufferedReader stdIn = new BufferedReader(br);
+		String st;
+		String ff = "";
+		while((st = br.readLine()) != null){
+			System.out.println(st);
+			ff += st;
 		}
-		out.close();
-		in.close();
-		clientSocket.close();
-		serverSocket.close();
-		
+		out.println(ff);	
 	}
+	out.close();
+	in.close();
+	clientSocket.close();
+	serverSocket.close();	
 }
+}
+/*class router{
+	static String FN = "";
+	public static String r(String c) {
+		switch(c){
+		case "animal":
+		FN = "animal.txt";
+			break;
+		case "employee":
+			FN = "employee.txt";	
+			break;
+		case "supply":
+			FN = "supply.txt";
+			break;
+		default:
+			break;
+	}
+		return FN;
+}
+}*/	
