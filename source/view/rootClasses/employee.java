80a72da69e73ddd20ac30aec80d174c0188a325d
Majeed-Omer <91630712+Majeed-Omer@users.noreply.github.com>
2021-11-20 15:27:12 +0300

    edited

--- source/view/rootClasses/employee.java
+++ source/view/rootClasses/employee.java
@@ -1,38 +1,43 @@
 package m;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class employee {
-    static Scanner s = new Scanner(System.in);
-    private static String nameE  = "";
+	static Scanner s = new Scanner(System.in);
+	private static String nameE  = "";
     private static String informationE = "";
-    public static void managerActions() throws IOException {
-        System.out.println("What you want to do with employees:\n* add - add an employee to list of employees\n* delete - remove an employee to list of employees\n* update  - replace an employee with another employee in list of employees\n* view - show list of employees\n* search - to find a specific employee\n* back - back to list of actions");
-        String chooseE = s.nextLine().toLowerCase();
-        switch (chooseE) {
-            case "add" -> employeeModel.addE();
-            case "delete" -> employeeModel.deleteE();
-            case "update" -> employeeModel.updateE();
-            case "view" -> employeeModel.viewE();
-            case "search" -> employeeModel.searchE();
-            case "back" -> {}
-            default -> {
-                System.out.println("We don't have this action in list of actions\n");
-                managerActions();
-            }
-        }
+	public static void managerActions() throws IOException, SQLException {
+    	System.out.println("What you want to do with employees:\n* add - add an employee to list of employees\n* delete - remove an employee to list of employees\n* update  - replace an employee with another employee in list of employees\n* view - show list of employees\n* search - to find a specific employee\n* back - back to list of actions");
+    	String chooseE = s.nextLine().toLowerCase();
+    	if(chooseE.equals("add")) {
+    		employeeModel.addE();
+    	} else if(chooseE.equals("delete")) {
+    		employeeModel.deleteE();
+    	}else if(chooseE.equals("update")) {
+    		employeeModel.updateE();
+    	}else if(chooseE.equals("view")) {
+    		employeeModel.viewE();
+    	}else if(chooseE.equals("search")) {
+    		employeeModel.searchE();
+    	}else if(chooseE.equals("back")) {
+    		
+    	}else {
+    		System.out.println("We don't have this action in list of actions\n");
+    		managerActions();
+    	}	
     }
-    public static void setNameE(String nE) {
-        nameE = nE;
-    }
-    public static String getNameE() {
-        return nameE;
-    }
-    public static void setInformationE(String infoE) {
-        informationE = infoE;
-    }
-    public static String getInformationE() {
-        return informationE;
-    }
-}
+	public static void setNameE(String nE) {
+		nameE = nE;
+	}
+	public static String getNameE() {
+		return nameE;
+	}
+	public static void setInformationE(String infoE) {
+		informationE = infoE;
+	}
+	public static String getInformationE() {
+		return informationE;
+	}
+ }
