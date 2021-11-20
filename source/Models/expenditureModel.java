f762b7bb56784c27625062c15a35cbc1b068155a
Yahya-Mewan <91800870+Yahya-Mewan@users.noreply.github.com>
2021-11-20 14:41:39 +0300

    Update expenditureModel.java
    updated and cleaned up

--- source/Models/expenditureModel.java
+++ source/Models/expenditureModel.java
@@ -1,28 +1,29 @@
 package m;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class expenditureModel {
-	static Scanner s = new Scanner(System.in);
-	static int price1 = 0;
-	public static void increaseX(int p) {
-		price1 += p;
-		expenditure.managerActions();
-	}
-	public static void decreaseX(int p) {
-		if(price1!=0)
-		price1 -= p;
-		else
-			System.out.println("Your expenditure is 0$\n");
-		expenditure.managerActions();
-	}
-    public static void clearX() {
-		price1 = 0;
-		System.out.println("The expenditure is: " + price1 + "$\n");
-		expenditure.managerActions();
-	}
-    public static void viewX() {
-		System.out.println("The expenditure is: " + price1 + "$\n");
-		expenditure.managerActions();
-	}
+    static Scanner s = new Scanner(System.in);
+    static int price1 = 0;
+    public static void increaseX(int p) throws IOException {
+        price1 += p;
+        expenditure.managerActions();
+    }
+    public static void decreaseX(int p) throws IOException {
+        if(price1!=0)
+            price1 -= p;
+        else
+            System.out.println("Your expenditure is 0$\n");
+        expenditure.managerActions();
+    }
+    public static void clearX() throws IOException {
+        price1 = 0;
+        System.out.println("The expenditure is: " + price1 + "$\n");
+        expenditure.managerActions();
+    }
+    public static void viewX() throws IOException {
+        System.out.println("The expenditure is: " + price1 + "$\n");
+        expenditure.managerActions();
+    }
 }
