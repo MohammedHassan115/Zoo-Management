3af73d2b5bac048fa2e5ca2d54dcdde117345917
Yahya-Mewan <91800870+Yahya-Mewan@users.noreply.github.com>
2021-11-20 14:29:48 +0300

    Update expenditure.java
    updated and cleaned up

--- source/view/rootClasses/expenditure.java
+++ source/view/rootClasses/expenditure.java
@@ -1,29 +1,31 @@
 package m;
 
+import java.io.IOException;
 import java.util.Scanner;
 public class expenditure {
-	static Scanner s = new Scanner(System.in);
-	
-	public static void managerActions() {
-		System.out.println("What you want to do with expenditures:\n* increase - you can increase expenditure with it\n* decrease - you can decrease expenditure with it\n* clear - expenditure will be (0)\n* view - show you how many expenditure we have\n* back - back to list of actions");
-		String chooseX = s.nextLine().toLowerCase();
-    	if(chooseX.equals("increase")) {
-    		System.out.print("Please enter the increased expenditure: ");
-    		int price2 = s.nextInt();
-    		expenditureModel.increaseX(price2);
-    	}else if(chooseX.equals("decrease")) {
-    		System.out.print("Please enter the decreased expenditure: ");
-    		int price2 = s.nextInt();
-    		expenditureModel.decreaseX(price2);
-    	}else if(chooseX.equals("clear")) {
-    		expenditureModel.clearX();
-    	}else if(chooseX.equals("view")) {
-    		expenditureModel.viewX();
-    	}else if(chooseX.equals("back")) {
-    		
-    	}else {
-    		System.out.println("We don't have this action in list of actions\n");
-    		managerActions();
-		}
-	}
+    static Scanner s = new Scanner(System.in);
+
+    public static void managerActions() throws IOException {
+        System.out.println("What you want to do with expenditures:\n* increase - you can increase expenditure with it\n* decrease - you can decrease expenditure with it\n* clear - expenditure will be (0)\n* view - show you how many expenditure we have\n* back - back to list of actions");
+        String chooseX = s.nextLine().toLowerCase();
+        switch (chooseX) {
+            case "increase" -> {
+                System.out.print("Please enter the increased expenditure: ");
+                int price2 = s.nextInt();
+                expenditureModel.increaseX(price2);
+            }
+            case "decrease" -> {
+                System.out.print("Please enter the decreased expenditure: ");
+                int price2 = s.nextInt();
+                expenditureModel.decreaseX(price2);
+            }
+            case "clear" -> expenditureModel.clearX();
+            case "view" -> expenditureModel.viewX();
+            case "back" -> {}
+            default -> {
+                System.out.println("We don't have this action in list of actions\n");
+                managerActions();
+            }
+        }
+    }
 }
