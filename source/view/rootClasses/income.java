0fbe2c9bdd8ab2290b62fcfa9b29bad7abfc6343
Yahya-Mewan <91800870+Yahya-Mewan@users.noreply.github.com>
2021-11-20 14:30:56 +0300

    Update income.java
    updated and cleaned up

--- source/view/rootClasses/income.java
+++ source/view/rootClasses/income.java
@@ -1,26 +1,26 @@
 package m;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class income {
-	static Scanner s = new Scanner(System.in);
-	
-	public static void managerActions() {
-		System.out.println("What you want to do with incomes:\n* update - update your income\n* clear - income will be (0)\n* view - show you how many income we have\n* back - back to list of actions");
-		String chooseI = s.nextLine().toLowerCase();
-    	if(chooseI.equals("update")) {
-    		System.out.print("Please enter the new income: ");
-    		int p = s.nextInt();
-    		incomeModel.updateI(p, "i");
-    	} else if(chooseI.equals("clear")) {
-    		incomeModel.clearI();
-    	}else if(chooseI.equals("view")) {
-    		incomeModel.viewI();
-    	}else if(chooseI.equals("view")) {
-    		
-    	}else {
-    		System.out.println("We don't have this action in list of actions\n");
-    		managerActions();
-    	}
-	}
+    static Scanner s = new Scanner(System.in);
+
+    public static void managerActions() throws IOException {
+        System.out.println("What you want to do with incomes:\n* update - update your income\n* clear - income will be (0)\n* view - show you how many income we have\n* back - back to list of actions");
+        String chooseI = s.nextLine().toLowerCase();
+        switch (chooseI) {
+            case "update" -> {
+                System.out.print("Please enter the new income: ");
+                int p = s.nextInt();
+                incomeModel.updateI(p, "i");
+            }
+            case "clear" -> incomeModel.clearI();
+            case "view" -> incomeModel.viewI();
+            case "back" -> {}
+            default -> {System.out.println("We don't have this action in list of actions\n");
+                managerActions();
+            }
+        }
+    }
 }
