6363e7dd662966da374538b2fc3985cf6d1c07cb
Yahya-Mewan <91800870+Yahya-Mewan@users.noreply.github.com>
2021-11-20 14:42:18 +0300

    Update incomeModel.java
    updated and cleaned up

--- source/Models/incomeModel.java
+++ source/Models/incomeModel.java
@@ -1,24 +1,25 @@
 package m;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class incomeModel {
-	static Scanner s = new Scanner(System.in);
-	static int price = 0;
-	public static void updateI(int p, String w) {
-		price += p;
-		if(w.equals("t"))
-			ticket.userActions();
-		else	
-		income.managerActions();
-	}
-    public static void clearI() {
-		price = 0;
-		System.out.println("The income is: " + price + "$\n");
-		income.managerActions();
-	}
-    public static void viewI() {
-		System.out.println("The income is: " + price + "$\n");
-		income.managerActions();
-	}
+    static Scanner s = new Scanner(System.in);
+    static int price = 0;
+    public static void updateI(int p, String w) throws IOException {
+        price += p;
+        if(w.equals("t"))
+            ticket.userActions();
+        else
+            income.managerActions();
+    }
+    public static void clearI() throws IOException {
+        price = 0;
+        System.out.println("The income is: " + price + "$\n");
+        income.managerActions();
+    }
+    public static void viewI() throws IOException {
+        System.out.println("The income is: " + price + "$\n");
+        income.managerActions();
+    }
 }
