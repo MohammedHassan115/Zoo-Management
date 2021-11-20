6feca14c9333562278d2d39cf793dc3a65b267f6
Yahya-Mewan <91800870+Yahya-Mewan@users.noreply.github.com>
2021-11-20 15:02:00 +0300

    Update client.java
    updated

--- source/Controller/client.java
+++ source/Controller/client.java
@@ -6,47 +6,47 @@ import java.util.*;
 
 // Client class
 class client {
-	
-	public client() {
-		// establish a connection by providing host and port
-		// number
-		try (Socket socket = new Socket("localhost", 6534)) {
-					
-			// writing to server
-		PrintWriter out = new PrintWriter(
-		socket.getOutputStream(), true);
 
-		// reading from server
-		BufferedReader in= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    public client() {
+        // establish a connection by providing host and port
+        // number
+        try (Socket socket = new Socket("localhost", 6534)) {
 
-		// object of scanner class
-		Scanner sc = new Scanner(System.in);
-		String line = null;
+            // writing to server
+            PrintWriter out = new PrintWriter(
+                    socket.getOutputStream(), true);
 
-		while (!"exit".equalsIgnoreCase(line)) {
-						
-		System.out.println("Which one to find it in server:\n* animal\n* employee\n* supply");
-		// reading from user
-		line = sc.nextLine();
+            // reading from server
+            BufferedReader in= new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-		// sending the user input to server
-		out.println(line);
-		out.flush();
+            // object of scanner class
+            Scanner sc = new Scanner(System.in);
+            String line = null;
 
-		// displaying server reply
-		System.out.println("Server replied "+ in.readLine());
-		}
-					
-		// closing the scanner object
-		sc.close();
-		}
-		catch (IOException e) {
-		e.printStackTrace();
-			}
-	}
-	// driver code
-	public static void main(String[] args)
-	{
-		new client();
-	}
+            while (!"exit".equalsIgnoreCase(line)) {
+
+                System.out.println("Which one to find it in server:\n* animal\n* employee\n* supply");
+                // reading from user
+                line = sc.nextLine();
+
+                // sending the user input to server
+                out.println(line);
+                out.flush();
+
+                // displaying server reply
+                System.out.println("Server replied "+ in.readLine());
+            }
+
+            // closing the scanner object
+            sc.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    // driver code
+    public static void main(String[] args)
+    {
+        new client();
+    }
 }
