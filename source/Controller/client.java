6e2d89d486ce4e406e74924185ef61617a6ef436
Majeed-Omer <91630712+Majeed-Omer@users.noreply.github.com>
2021-11-28 18:59:01 +0300

    edited

--- source/Controller/client.java
+++ source/Controller/client.java
@@ -4,49 +4,50 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
+
 // Client class
 class client {
+	
+	public client() {
+		// establish a connection by providing host and port
+		// number
+		try (Socket socket = new Socket("localhost", 1234)) {
+					
+			// writing to server
+		PrintWriter out = new PrintWriter(
+		socket.getOutputStream(), true);
 
-    public client() {
-        // establish a connection by providing host and port
-        // number
-        try (Socket socket = new Socket("localhost", 6534)) {
+		// reading from server
+		BufferedReader in= new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            // writing to server
-            PrintWriter out = new PrintWriter(
-                    socket.getOutputStream(), true);
+		Scanner sc = new Scanner(System.in);
+		String line = null;
 
-            // reading from server
-            BufferedReader in= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		while (!"exit".equalsIgnoreCase(line)) {
+						
+		System.out.println("Which one to find it in server:\n* animals\n* employees\n* supplies");
+		// reading from user
+		line = sc.nextLine();
 
-            // object of scanner class
-            Scanner sc = new Scanner(System.in);
-            String line = null;
-
-            while (!"exit".equalsIgnoreCase(line)) {
-
-                System.out.println("Which one to find it in server:\n* animal\n* employee\n* supply");
-                // reading from user
-                line = sc.nextLine();
-
-                // sending the user input to server
-                out.println(line);
-                out.flush();
-
-                // displaying server reply
-                System.out.println("Server replied "+ in.readLine());
-            }
-
-            // closing the scanner object
-            sc.close();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    // driver code
-    public static void main(String[] args)
-    {
-        new client();
-    }
+		if(line.equals("animals") || line.equals("employees") || line.equals("supplies")) {
+			out.println(line);
+			out.flush();
+			System.out.println("Server replied: "+ in.readLine() + "\n");
+		}else {
+			System.out.println("Please select one of the choose from the list\n");
+		}
+		
+		}
+					
+		sc.close();
+		}
+		catch (IOException e) {
+		e.printStackTrace();
+			}
+	}
+	// driver code
+	public static void main(String[] args)
+	{
+		new client();
+	}
 }
