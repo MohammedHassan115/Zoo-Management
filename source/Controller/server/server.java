782f417e909ca899f60de0dcbaf5c6e49ffb6979
Majeed-Omer <91630712+Majeed-Omer@users.noreply.github.com>
2021-11-28 18:59:46 +0300

    edited

--- source/Controller/server/server.java
+++ source/Controller/server/server.java
@@ -1,118 +1,157 @@
 package pop;
 
+
 import java.io.*;
 import java.net.*;
+import java.sql.*;
+import java.util.ArrayList;
 
 // Server class
 class server {
-    public static void main(String[] args)
-    {
-        ServerSocket server = null;
+	static String url = "jdbc:mysql://localhost:3306/zoo";
+	static String uname = "root";
+	static String password = "12345";
+	// static String query = "";
+	static Connection con = null;
+	public static void main(String[] args)
+	{
+		ServerSocket server = null;
 
-        try {
+		try {
 
-            // server is listening on port 6534
-            server = new ServerSocket(6534);
-            server.setReuseAddress(true);
+			// server is listening on port 1234
+			server = new ServerSocket(1234);
+			server.setReuseAddress(true);
 
-            // running infinite loop for getting
-            // client request
-            while (true) {
+			// running infinite loop for getting
+			// client request
+			while (true) {
 
-                // socket object to receive incoming client
-                // requests
-                Socket client = server.accept();
+				// socket object to receive incoming client
+				// requests
+				Socket client = server.accept();
 
-                // Displaying that new client is connected
-                // to server
-                System.out.println("New client connected"
-                        + client.getInetAddress()
-                        .getHostAddress());
+				// Displaying that new client is connected
+				// to server
+				System.out.println("New client connected"
+								+ client.getInetAddress()
+										.getHostAddress());
 
-                // create a new thread object
-                ClientHandler clientSock
-                        = new ClientHandler(client);
+				// create a new thread object
+				ClientHandler clientSock
+					= new ClientHandler(client);
 
-                // This thread will handle the client
-                // separately
-                new Thread(clientSock).start();
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        finally {
-            if (server != null) {
-                try {
-                    server.close();
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+				// This thread will handle the client
+				new Thread(clientSock).start();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			if (server != null) {
+				try {
+					server.close();
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-    // ClientHandler class
-    private static class ClientHandler implements Runnable {
-        private final Socket clientSocket;
+	// ClientHandler class
+	private static class ClientHandler implements Runnable {
+		private final Socket clientSocket;
 
-        // Constructor
-        public ClientHandler(Socket socket)
-        {
-            this.clientSocket = socket;
-        }
+		// Constructor
+		public ClientHandler(Socket socket)
+		{
+			this.clientSocket = socket;
+		}
 
-        public void run()
-        {
-            PrintWriter out = null;
-            BufferedReader in = null;
-            try {
+		public void run()
+		{
+			PrintWriter out = null;
+			BufferedReader in = null;
+			try {
+					
+				// get the outputstream of client
+				out = new PrintWriter(
+					clientSocket.getOutputStream(), true);
 
-                // get the outputstream of client
-                out = new PrintWriter(
-                        clientSocket.getOutputStream(), true);
+				// get the inputstream of client
+				in = new BufferedReader(
+					new InputStreamReader(
+						clientSocket.getInputStream()));
 
-                // get the inputstream of client
-                in = new BufferedReader(
-                        new InputStreamReader(
-                                clientSocket.getInputStream()));
+				String line;
+				while ((line = in.readLine()) != null) {
 
-                String line;
-                while ((line = in.readLine()) != null) {
-
-
-                    // writing the received message from
-                    // client
-                    System.out.printf("Sent from the client: %s\n",line);
-                    FileWriter fw = new FileWriter(line + ".txt", true);
-                    File f = new File("C:\\Users\\PC-BALEN\\eclipse-workspace\\mom\\" + line + ".txt");
-                    BufferedReader br = new BufferedReader(new FileReader(f));
-                    String st;
-                    String ff = "";
-                    while((st = br.readLine()) != null){
-                        ff += st;
-                    }
-                    out.println(ff);
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-            finally {
-                try {
-                    if (out != null) {
-                        out.close();
-                    }
-                    if (in != null) {
-                        in.close();
-                        clientSocket.close();
-                    }
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+					
+					// writing the received message from
+					// client
+					System.out.printf("Sent from the client: %s\n",line);
+					if(line.equals("supplies")) {
+					String query = String.format("select * from %s", line);
+						try {
+							con = DriverManager.getConnection(url, uname, password);
+							Statement statement = con.createStatement();
+							ResultSet result = statement.executeQuery(query);
+							ArrayList<String> arraylist = new ArrayList<String>();
+							while(result.next()) {
+								String names  = result.getString("names");
+								String quantity  = result.getString("quantities");
+								String price  = result.getString("prices");
+								 arraylist.add(names + ": " + quantity + ": " + price);
+							}
+							statement.close();
+							con.close();
+							out.println(arraylist);
+						}catch(SQLException e) {
+							e.printStackTrace();
+						}
+				    	System.out.println();
+					}else {
+					   String query = String.format("select * from %s", line);	
+					   try {
+					   con = DriverManager.getConnection(url, uname, password);
+					   Statement statement = con.createStatement();
+					   ResultSet result = statement.executeQuery(query);
+					   ArrayList<String> arraylist = new ArrayList<String>();
+					   while(result.next()) {
+						 String names  = result.getString("names");
+						 String informations  = result.getString("information");
+						 arraylist.add(names + ": " + informations);
+						}
+					   statement.close();
+					   con.close();
+					   out.println(arraylist);
+					}catch(SQLException e) {
+						e.printStackTrace();
+					}
+			    	System.out.println();
+					}
+					
+				}
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					if (out != null) {
+						out.close();
+					}
+					if (in != null) {
+						in.close();
+						clientSocket.close();
+					}
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
